6bb0fa746cd6c6dccc63123b92f7d81addca7b2c
daanniill <daniil.malakhov12@gmail.com>
2025-08-06 16:31:41 -0700

    added functionality to number buttons and added styling to numbers on calculator screen

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-function input() {
-    const curInput = new Object()
-    
+let currentInput = ''
+let currentOperation = ''
+let previousInput = ''
+
+function appendNumber(number) {
+    currentInput += number
+    document.getElementsByClassName("screen")[0].innerText = `${previousInput} ${currentOperation} ${currentInput}`
+
 }
+
