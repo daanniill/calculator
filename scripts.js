74757da2c295ca48b659ce1b990ff3969e347606
daanniill <daniil.malakhov12@gmail.com>
2025-08-06 16:53:08 -0700

    added logic to calculator. implemented calculate function in js where it processes inputted operations

--- scripts.js
+++ scripts.js
@@ -10,8 +10,44 @@ function appendNumber(number) {
 
 function appendOperation(operation) {
     if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate(); 
+    }
     currentOperation = operation;
     previousInput = currentInput;
     currentInput = '';
     document.getElementsByClassName("screen")[0].innerText = `${previousInput} ${currentOperation}`
+}
+
+function calculate() {
+    if (previousInput === '' || currentInput === '') return;
+    let result;
+    let prev = parseFloat(previousInput);
+    let current = parseFloat(currentInput);
+
+    switch (currentOperation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            if (current === 0) {
+                alert("Cannot divide by zero");
+                return;
+            }
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    currentInput = result.toString();
+    currentOperation = '';
+    previousInput = '';
+    document.getElementsByClassName("screen")[0].innerText = currentInput;
 }
